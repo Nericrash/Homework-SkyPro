e9c6d1e37074fa8af070d48d491e700352fc79f9
YakovIsikeev <Barcas48ru@gmail.com>
2024-07-19 13:19:00 +0300

    flake8 and PEP-8 changes

--- src/masks.py
+++ src/masks.py
@@ -1,27 +1,31 @@
-from typing import List
 import logging
+from typing import List
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s",
-    filename="../logs/mask.log",
-    filemode="w",
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/masks.log")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s", "%d.%m.%Y %H:%M:%S"
 )
-
-logger = logging.getLogger("mask")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_mask_card_number(number_card: List[int]) -> str:
     """Функция принимает на вход номер карты и возвращает маскированную."""
     str_card_number = str(number_card)
-    logger.info(f"Card mask: {str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}")
-    return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+    try:
+        logger.info(f"Card mask: {str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}")
+        return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+    except Exception:
+        logger.error("Error")
 
 
 def get_mask_account(account_number: List[int]) -> str:
     """Функция принимает на вход номер счета и возвращает замаскированный"""
     str_account_number = str(account_number)
-    logger.info(f"Account mask: **{str_account_number[-4:]}")
-    return f"**{str_account_number[-4:]}"
-# mask_account_number = "**" + str_account_number[-4:]
-# return mask_account_number
+    try:
+        logger.info(f"Account mask: **{str_account_number[-4:]}")
+        return f"**{str_account_number[-4:]}"
+    except Exception:
+        logger.error("Error")
